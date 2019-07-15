3b9d125d0e67c02be8c9c73bdcd3ac6df5588b5a
Russ Allbery <rra@cpan.org>
2019-07-14 20:48:56 -0700

    Release 3.01

--- lib/App/DocKnot/Dist.pm
+++ lib/App/DocKnot/Dist.pm
@@ -10,7 +10,7 @@
 # Modules and declarations
 ##############################################################################
 
-package App::DocKnot::Dist 3.00;
+package App::DocKnot::Dist 3.01;
 
 use 5.024;
 use autodie;
