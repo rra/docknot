ceed1ea14819282482129ad02482cdeb5b4432f4
Russ Allbery <rra@cpan.org>
2022-01-16 20:25:32 -0800

    Use edit_lines in App::DocKnot::Spin::Versions
    Use edit_lines_utf8 instead of edit_utf8 to modify the .versions
    file, since it simplifies the logic.

--- lib/App/DocKnot/Spin/Versions.pm
+++ lib/App/DocKnot/Spin/Versions.pm
@@ -168,34 +168,30 @@ sub update_version {
 
     # Edits the line for the package to replace the version and release date.
     my $edit = sub {
-        my @lines = split(m{ \n }xms);
-        for my $line (@lines) {
-            my ($product, $old_version, $old_date, $old_time)
-              = split(q{ }, $line);
-            next if $product ne $package;
+        my ($product, $old_version, $old_date, $old_time) = split(q{ });
+        return if $product ne $package;
 
-            # We're going to replace the old version with the new one, but we
-            # need to space-pad one or the other if they're not the same
-            # length.
-            my $version_string = $version;
-            while (length($old_version) > length($version_string)) {
-                $version_string .= q{ };
-            }
-            while (length($old_version) < length($version_string)) {
-                $old_version .= q{ };
-            }
-
-            # Make the replacement.
-            $line =~ s{ \Q$old_version\E }{$version_string}xms;
-            $line =~ s{ \Q$old_date\E }{$date}xms;
-            $line =~ s{ \Q$old_time\E }{$time}xms;
+        # We're going to replace the old version with the new one, but we need
+        # to space-pad one or the other if they're not the same length.
+        my $version_string = $version;
+        while (length($old_version) > length($version_string)) {
+            $version_string .= q{ };
         }
-        $_ = join("\n", @lines) . "\n";
+        while (length($old_version) < length($version_string)) {
+            $old_version .= q{ };
+        }
+
+        # Make the replacement.
+        my $line = $_;
+        $line =~ s{ \Q$old_version\E }{$version_string}xms;
+        $line =~ s{ \Q$old_date\E }{$date}xms;
+        $line =~ s{ \Q$old_time\E }{$time}xms;
+        $_ = $line;
     };
 
     # Apply that change to our versions file, and then re-read the contents to
     # update the internal data structure.
-    $self->{path}->edit_utf8($edit);
+    $self->{path}->edit_lines_utf8($edit);
     $self->_read_data();
     return;
 }
